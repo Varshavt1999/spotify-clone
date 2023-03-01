b6e2213ac1efbc5f7ec2d5c81ddb5f1b08c8da3d
Varshavt1999 <varshavt7399@gmail.com>
2023-03-01 17:12:57 +0530

    warning removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reducer, { initialState } from "./context/Reducer";
 import Store from "./context/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
